c813e11882917f4e52cd5c80f2474d6f319d7701
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-09-01 16:54:55 -0300

    conexxion con react router y colocacion de iconos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.css';
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1 className="Header-title">Conf merch</h1>
-      <div className="Header-checkout">Checkout</div>
+      <h1 className="Header-title">
+          <Link to="/">
+            Platzi Conf merch
+          </Link>
+        </h1>
+      <div className="Header-checkout">
+          <Link to={"/checkout"}>
+          <i className="fas fa-shopping-basket fa-2x" title="Checkout" />
+          </Link>
+        </div>
     </div>
   );
 };
